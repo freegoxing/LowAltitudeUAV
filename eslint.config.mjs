1929e2167b42551325c70db90f154dc7eb320c87
free <mpdf2023@outlook.com>
2026-07-08 22:02:41 +0800

    chore: excluade python file in next.js project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,10 +8,17 @@ const eslintConfig = defineConfig([
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+	  ".next/**",
+	  "out/**",
+	  "build/**",
+	  "next-env.d.ts",
+	  // Python and data directories
+	  "src/uav_semantic_planner/**",
+	  ".venv/**",
+	  "checkpoints/**",
+	  "data/**",
+	  "visualizations/**",
+	  "docs/**"
   ]),
 ]);
 
