39b983c40623e1d8dc601c71b4131dc9eee1e0e8
free <mpdf2023@outlook.com>
2026-07-15 10:49:08 +0800

    chore: excluade .nextjs/ for web gui

--- apps/web/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"0EbFXyqjKuDpytyYKLi+BK1+ZVQfS/M+QJDa2HVHoXQ=\"\n}"
--- apps/web/.next/dev/static/chunks/apps_web_1anvha4._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_15_68vi._.js","static/chunks/0-cn_next_dist_compiled_next-devtools_index_0is-pyx.js","static/chunks/0-cn_next_dist_compiled_react-dom_1pfrkuj._.js","static/chunks/0-cn_next_dist_compiled_react-server-dom-turbopack_153d0bx._.js","static/chunks/0-cn_next_dist_compiled_1yw067a._.js","static/chunks/0-cn_next_dist_client_0sc3_gt._.js","static/chunks/0-cn_next_dist_0yobqhu._.js","static/chunks/0vvp_@swc_helpers_cjs_0vb02wp._.js"],
-    source: "entry"
-});
